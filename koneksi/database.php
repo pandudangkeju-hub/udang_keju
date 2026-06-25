79350473db55b8ed1d22c3e7513a0e2a2620f679
pandudangkeju <pandurendang@gmail.com>
2026-06-25 12:46:00 +0700

    database

---
+++ koneksi/database.php
@@ -0,0 +1,21 @@
+<?php
+class Database {
+    private $host = "localhost";
+    private $db_name = "DB_SIMULASI_PBO_KELAS_NamaLengkap"; // Sesuaikan nama DB Anda
+    private $username = "root";
+    private $password = "";
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        } catch(PDOException $exception) {
+            echo "Koneksi basis data gagal: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
+?>
